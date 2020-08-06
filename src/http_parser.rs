f1f119f663a29f97a1b9a398940213395cbfa684
memir0 <memir0@hotmail.no>
2020-08-06 22:33:58 +0200

    parser complete but not tested, main code WIP

--- src/http_parser.rs
+++ src/http_parser.rs
@@ -1,51 +1,112 @@
+use crate::request;
 use std::collections::HashMap;
-use std::io::{BufRead, BufReader, Read, Write};
+use std::io::{BufRead, BufReader, Read};
 use std::net::TcpStream;
-mod request;
 
 pub struct HttpParser {}
 
 impl HttpParser {
     pub fn new() -> HttpParser {
-        return HttpParser;
+        return HttpParser {};
     }
 
     pub fn parse(stream: TcpStream) -> Result<request::Request, String> {
         let mut reader = BufReader::new(stream);
         // Read first line
-        let _result = reader.by_ref().read_line(&mut http_request_definition);
-        let http_request_definition_split: Vec<&str> = http_request_definition.split_whitespace().collect();
+        let mut http_request_line = String::new();
+        let _result = reader.by_ref().read_line(&mut http_request_line);
+        let http_request_line_split: Vec<&str> = http_request_line.split_whitespace().collect();
+
+        // Input validation
+        if http_request_line_split.len() != 3 {
+            return Err(format!(
+                "Request line not correct syntax: {}",
+                http_request_line
+            ));
+        }
 
         // Process headers and body
-        let mut http_request_headers = HashMap::new(String, String);
-        let mut body_bytes: Vec<u8> = vec![];
-        
+        let mut http_request_headers = HashMap::new();
         for line_result in reader.by_ref().lines() {
             let line = match line_result {
                 Ok(line_string) => line_string,
-                Err(error) => return Err(Failed to read line from TCP stream)
+                Err(error) => String::from("ERROR"),
+            };
+            if line == "ERROR" {
+                return Err(String::from("Failed to read line from TCP stream"));
             }
             let mut iter = line.split(": ");
             let key = match iter.next() {
-                Ok(result) => result,
-                Err(error) => return Err("Faulty request syntax, could not parse")
+                Some(result) => result,
+                None => "Error: no key",
+            };
+            if key == "Error: no key" {
+                return Err(String::from("Faulty request syntax, could not parse"));
             }
             let value = match iter.next() {
-                Ok(result) => result,
-                Err(error) => return Err("Faulty request syntax, could not parse")
+                Some(result) => result,
+                None => "Error: no value",
+            };
+            if value == "Error: no value" {
+                return Err(String::from("Faulty request syntax, could not parse"));
             }
-            http_request_headers.insert(key, value);
+            http_request_headers.insert(key.to_lowercase(), String::from(value));
         }
-        if has_body {
-            let body_length = match .parse(){
+        let mut body = String::from("");
 
+        // If content lenght header is set we assume it has a body and try to read it
+        if http_request_headers.contains_key("content-length") {
+            let body_length: i32 = match http_request_headers["content-length"].parse() {
+                Ok(result) => result,
+                Err(_error) => -1,
+            };
+            if body_length > 0 {
+                return Err(String::from("Invalid content-legth header"));
             }
-            body_bytes = vec![0; body_length];
-            let result = reader.by_ref().read_exact(&mut body);
-            match result {
-                Ok() => {}
-                Err(error) => {println!("Read request body failed: ", error)}
+            let mut body_bytes = vec![0; body_length as usize];
+            let result = reader.by_ref().read_exact(&mut body_bytes);
+            let fail = match result {
+                Ok(_result) => false,
+                Err(_error) => true,
+            };
+            if fail {
+                return Err(String::from("Read request body failed"));
+            }
+            for char_byte in body_bytes {
+                body.push(char_byte as char);
             }
         }
+
+        // Get parameters from request
+        let url_split: Vec<&str> = http_request_line_split[1].split("?").collect();
+        let mut parameters = HashMap::new();
+        // If the url has parameters set, get them
+        if url_split.len() > 1 {
+            let parameters_vec: Vec<&str> = url_split[1].split("&").collect();
+            for parameter in parameters_vec {
+                let parameter_split: Vec<&str> = parameter.split("=").collect();
+                if parameter_split.len() > 1 {
+                    parameters.insert(
+                        String::from(parameter_split[0]),
+                        String::from(parameter_split[1]),
+                    );
+                }
+            }
+        }
+
+        let mut http_version = String::from("1.1");
+        let version_split: Vec<&str> = http_request_line_split[2].split("/").collect();
+        if version_split.len() > 1 {
+            http_version = String::from(version_split[1]);
+        }
+
+        Ok(request::Request::new(
+            String::from(http_request_line_split[1]),
+            parameters,
+            body,
+            http_version,
+            String::from(http_request_line_split[0]),
+            http_request_headers,
+        ))
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 use std::collections::HashMap;
+use std::net::{TcpListener, TcpStream};
 use std::sync::{Arc, Condvar, Mutex};
 use std::thread;
+mod http_parser;
 mod request;
 mod response;
 
 pub struct Spot {
     threads: Vec<std::thread::JoinHandle<()>>,
-    working_threads_arc: Arc<(Mutex<u32>, Condvar)>,
+    stream_storage: Vec<Arc<Vec<Option<TcpStream>>>>,
+    stream_locks: Vec<Arc<(Mutex<bool>, Condvar)>>,
     amount_of_threads: u32,
     routes: HashMap<String, fn(request::Request, response::Response) -> bool>,
 }
@@ -15,11 +18,23 @@ impl Spot {
     pub fn new(amount_of_threads: u32) -> Spot {
         // Start worker threads
         let mut threads = Vec::new();
+        let mut stream_storage = Vec::new();
+        let mut stream_locks = Vec::new();
         for i in 0..amount_of_threads {
+            let stream_lock = Arc::new((Mutex::new(true), Condvar::new()));
+            let none: Option<TcpStream> = None;
+            let stream_option = Arc::new(none);
+            stream_locks.push(stream_lock.clone());
+            stream_storage.push(stream_option.clone());
             let new_worker = thread::Builder::new()
                 .name(format!("Spot-Worker-{}", i + 1))
-                .spawn(move || {
-                    print!("Hello");
+                .spawn(move || loop {
+                    let (lock, condvar) = &*stream_lock;
+                    let mut waiting = lock.lock().unwrap();
+                    while *waiting {
+                        waiting = condvar.wait(waiting).unwrap();
+                    }
+                    // TODO: release lock, handle stream, set waiting to true
                 });
             match new_worker {
                 Ok(thread) => threads.push(thread),
@@ -30,7 +45,8 @@ impl Spot {
         }
         return Spot {
             threads: threads,
-            working_threads_arc: Arc::new((Mutex::new(0u32), Condvar::new())),
+            stream_storage: Vec::new(),
+            stream_locks: Vec::new(),
             amount_of_threads: amount_of_threads,
             routes: HashMap::new(),
         };
@@ -48,9 +64,39 @@ impl Spot {
         return true;
     }
 
-    pub fn bind(self, ip: String) {
+    pub fn bind(self, ip: &str) -> Result<bool, String> {
+        let listener = match TcpListener::bind(ip) {
+            Ok(result) => result,
+            Err(error) => {
+                return Err(String::from("Failed to bind to ip"));
+            }
+        };
+        let stream_distributor = match thread::Builder::new()
+            .name(String::from("Spot-Distributor"))
+            .spawn(move || {
+                for stream in listener.incoming() {
+                    let stream = match stream {
+                        Ok(stream) => stream,
+                        Err(error) => {
+                            println!("Tcp error: {}", error);
+                            return;
+                        }
+                    };
+                    // TODO: assign stream to waiting worker
+                }
+            }) {
+            Ok(handle) => handle,
+            Err(error) => {
+                return Err(String::from(format!(
+                    "Failed to start distributor thread: {}",
+                    error
+                )))
+            }
+        };
         for thread in self.threads {
             let _result = thread.join();
         }
+        let _result = stream_distributor.join();
+        Ok(true)
     }
 }
